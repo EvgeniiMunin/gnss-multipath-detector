f5903575f1dfa7443a78c085a46a5106ab28023f
root <root@portablese52.localdomain>
2021-03-23 11:23:57 +0100

    iteration loop for multipath generation

--- run_acquisition_sampling.sh
+++ run_acquisition_sampling.sh
@@ -10,8 +10,12 @@ echo "acquisition completed"
 
 # execute run_data_sampler.py. generate mp/no_mp
 cd ..
-python3 run_data_sampler.py --mp=0 --nb_samples=1 --cn0=48 --discr=80
-python3 run_data_sampler.py --mp=1 --nb_samples=1 --cn0=48 --discr=80
+for i in `seq 1 10`;
+do
+	python3 run_data_sampler.py --mp=0 --nb_samples=1 --cn0=48 --discr=80
+	python3 run_data_sampler.py --mp=1 --nb_samples=1 --cn0=48 --discr=80
+	echo $i
+done
 echo "sampling/ sum completed"
 
 exit 0
