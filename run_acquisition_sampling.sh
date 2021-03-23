8e846ad85adcf670544626519cc2a591e6489bf2
root <root@portablese52.localdomain>
2021-03-23 10:35:14 +0100

    Modification test (only 2 images are generated)

--- run_acquisition_sampling.sh
+++ run_acquisition_sampling.sh
@@ -5,13 +5,13 @@ chmod +x ./run_acquisition_sampling.sh
 
 # execute run_realiz_fake_noise.m
 cd datagen/corr_noise_gen/
-octave run_realiz_fake_noise.m 400 1
+octave run_realiz_fake_noise.m 20 1
 echo "acquisition completed"
 
 # execute run_data_sampler.py. generate mp/no_mp
 cd ..
-python3 run_data_sampler.py --mp=0 --nb_samples=200 --cn0=48 --discr=80
-python3 run_data_sampler.py --mp=1 --nb_samples=200 --cn0=48 --discr=80
+python3 run_data_sampler.py --mp=0 --nb_samples=1 --cn0=48 --discr=80
+python3 run_data_sampler.py --mp=1 --nb_samples=1 --cn0=48 --discr=80
 echo "sampling/ sum completed"
 
 exit 0
