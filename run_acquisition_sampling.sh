a52bf134d2e2dfeaacee1d9c9ee221aa7a776fee
root <root@portablese52.localdomain>
2021-04-02 16:03:19 +0200

    iteration loop to generate several signals but with different doppler values

--- run_acquisition_sampling.sh
+++ run_acquisition_sampling.sh
@@ -12,8 +12,13 @@
 # execute run_data_sampler.py. generate mp/no_mp
 #cd ..
 cd datagen/
-python3 run_data_sampler.py --mp=0 --nb_samples=1 --dopp=200 --tau=1.5 --cn0=43 --discr=80
-python3 run_data_sampler.py --mp=1 --nb_samples=1 --dopp=200 --tau=1.5 --cn0=43 --discr=80
+for _ in `seq 1 4`;
+do
+	rand=$(echo "scale=10;($RANDOM/32768)*501 - 250"|bc -l)
+	doppler=${rand%.*}
+	#python3 run_data_sampler.py --mp=0 --nb_samples=1 --dopp=200 --tau=1.5 --cn0=43 --discr=80
+	python3 run_data_sampler.py --mp=1 --nb_samples=1 --dopp=$doppler --tau=1.5 --cn0=43 --discr=80
+done
 #=======
 #cd datagen/corr_noise_gen/
 #octave run_realiz_fake_noise.m 20 1
